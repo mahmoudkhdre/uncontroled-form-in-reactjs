d6db1ce81a7bc79fb01b7980fdaf3d1ea1ef28f3
mahmoud khdre <mahmoudkhdre9@gmail.com>
2020-05-08 22:48:42 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+  class App extends Component{
+    state={
+      name:'',
+      pass:''
+    }
+    handleChange=(event)=> {
+       const target = event.target;
+        const value =  target.value;
+        const name = target.name;
+    
+        this.setState({
+          [name]: value
+        });
+ 
+  
+   event.preventDefault();
+  
+   
 }
+handleSubmit = (event)=>{
+  
+
+alert(` your name is ${this.state.name} and you pass is${this.state.pass}`);
+ event.preventDefault();
+ 
+}
+    render(){
+ return (
+    
+      <form onSubmit={this.handleSubmit}>
+     <input type="text" name="name" id="name" value={this.state.name} placeholder="username"
+    onChange={this.handleChange}/>
+    <input type="text" name="pass" id="pass" value={this.state.pass} placeholder="password"
+    onChange={this.handleChange}/>
+
+    <button type="submit" color="primary"> login</button>
+    </form>
+    
+    
+  );
+    }
+  }
+ 
+
 
 export default App;
